dc0d8f6ebf71332236702ee78b15030658064c33
ahmad-buhari <at.buhari@gmail.com>
2021-03-26 13:21:52 +0000

    Updates, added AD creation

--- script.ps1
+++ script.ps1
@@ -1,40 +1,50 @@
 #Function: Customize Windows Server Lab
+#Drafting initial lab prep workflow
 
 
 
+#Step1 
 #New Name
 $name = Read-Host -Prompt "`nNew Machine Name"
 Rename-Computer -NewName $name -WarningAction SilentlyContinue
 
-
-#Update PowerShell Help
-Write-Host "`nUpdating Powershell Help" -ForegroundColor Yellow
-$updateA = Update-Help -Force 
-$updateA
-
-#Installing Update Module
-Write-Host "`nInstalling PSWindowUpdateModule" -ForegroundColor Yellow
-$installerA = Install-Module -Name PSWindowsUpdate -Verbose -WarningAction SilentlyContinue
-$installerA
-
-#Installing Update
-$updateB = Install-WindowsUpdate -AcceptAll -IgnoreReboot
-$updateB
-
-#Get-IPaddress
-Write-host "`nLooking IPaddress"
-$ip = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet0
-Write-Host "`nIPaddress is" $ip.IPAddress "on interface" $ip.InterfaceIndex -ForegroundColor Yellow
-
+#Step2
 #Configure Network 
 $ipNew = Read-Host "`nEnter new IPaddress" ;
 $gateNew = Read-Host "`nEnter new subnet" ;
 $netConfigure = New-NetIPAddress -InterfaceIndex $ip.InterfaceIndex -IPAddress $ipNew -PrefixLength 24 -DefaultGateway $gateNew ;
 $netConfigure
 
-#Set DNS
+#Setup DNS
 $dns = Set-DnsClientServerAddress -InterfaceIndex $ip.InterfaceIndex -ServerAddresses $ipnew,"8.8.8.8"
 $dns
+
+
+#Get-IPaddress
+Write-host "`nLooking IPaddress"
+$ip = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet0
+Write-Host "`nIPaddress is" $ip.IPAddress "on interface" $ip.InterfaceIndex -ForegroundColor Yellow
+
+
+#Step2
+#Update PowerShell Help
+Write-Host "`nUpdating Powershell Help" -ForegroundColor Yellow
+$updateHelp = Update-Help -Force 
+$updateHelp
+
+#Step3
+#Installing Windows Update PSModule
+Write-Host "`nInstalling PSWindowUpdateModule" -ForegroundColor Yellow
+$installerA = Install-Module -Name PSWindowsUpdate -Verbose -WarningAction SilentlyContinue
+$installerA
+
+#Step4
+#Installing Updates for Windows
+$updateB = Install-WindowsUpdate -AcceptAll -IgnoreReboot
+$updateB
+
+
+
 
 #Install AD Services
 Write-host "Installing AD Feature"
@@ -55,7 +65,45 @@ $ADprep2 = Install-ADDSForest `
 -NoRebootOnCompletion:$true `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
-
 $ADprep2
+
+
+#AD Base Prep
+
+$newBaseOU = Read-Host "Enter Base Name"
+
+    #New OrganizationalUnit Base
+    New-ADOrganizationalUnit `
+    -Name $newBaseOU `
+    -Path "DC=$domain,DC=LAB" `
+    -ProtectedFromAccidentalDeletion $false
+
+    #New OrganizationalUnit Computer
+    New-ADOrganizationalUnit `
+    -Name $newBaseOU-Computers `
+    -Path "OU=$domain,DC=AREA52,DC=LAB" `
+
+    -ProtectedFromAccidentalDeletion $false
+
+    #New OrganizationalUnit Users
+    New-ADOrganizationalUnit `
+    -Name $newBaseOU-Users `
+    -Path "OU=$domain,DC=AREA52,DC=LAB" `
+    -ProtectedFromAccidentalDeletion $false
+
+
+#Creating Lab Users, Compuers
+$fetch = Get-Content '.\Desktop\computer list.csv'
+
+$comps = @($fetch)
+
+
+#Add Comptuer Objects
+foreach ($computer in $comps) {New-ADComputer -Name $computer -Path "OU= M-Computers,OU=Mildenhall,DC=AREA52,DC=LAB" }
+
+
+#Remove Objects
+foreach ($computer in $comps){Remove-ADComputer -Identity $computer -Confirm:$false}
+
 
 
